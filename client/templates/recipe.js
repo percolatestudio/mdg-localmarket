1f9848b1a526844e74fc754fdc658520954dc814
Tom Coleman <tom@thesnail.org>
2014-08-15 13:23:55 +1000

    Use different icon for favorites.
    @dominicnguyen - we can use momentum to animate this if you’d like ;)

--- client/templates/recipe.js
+++ client/templates/recipe.js
@@ -5,6 +5,13 @@ Template.recipe.events({
     // XXX: methods?
     Recipes.update(this._id, {$set: {favorite: true}});
   },
+
+  'click [data-unfavorite]': function(e) {
+    e.preventDefault();
+    
+    // XXX: methods?
+    Recipes.update(this._id, {$unset: {favorite: true}});
+  },
   
   'click [data-ingredients-trigger]': function(e, template) {
     // XXX: @zol -- we should split velocity out as a separate package and
