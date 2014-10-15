ae92deb443ad3f3cd4b38b765bdce21800f63175
Tom Coleman <tom@thesnail.org>
2014-10-15 17:54:23 +1100

    New IA

--- server/publications.js
+++ server/publications.js
@@ -1,24 +1,16 @@
+Meteor.publish('allRecipes', function() {
+  return Recipes.find();
+});
+
+
 Meteor.publish('featuredRecipes', function() {
   return Recipes.featured();
 });
 
-Meteor.publish('favoriteRecipes', function() {
-  return Recipes.favorite();
+Meteor.publish('bookmarkedRecipes', function() {
+  check(this.userId, String)
+  return Recipes.bookmarked(this.userId);
 });
-
-
-Meteor.publish('seasonCounts', function() {
-  var self = this;
-  _.each(Recipes.SEASONS, function(season) {
-    Counts.publish(self, season.name, Recipes.forSeason(season.name));
-  });
-});
-
-Meteor.publish('seasonRecipes', function(name) {
-  check(name, String);
-  return Recipes.forSeason(name);
-});
-
 
 Meteor.publish('recipe', function(id) {
   check(id, String);
