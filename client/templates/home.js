d830c66c1ae20b58b36ff46642140515fbd907c3
Zoltan Olah <zol@percolatestudio.com>
2014-10-17 15:01:42 -0700

    Adds news collection

--- client/templates/home.js
+++ client/templates/home.js
@@ -5,5 +5,10 @@ Template.home.helpers({
   
   activities: function() {
     return Activities.recent();
+  },
+  
+  latestNews: function() {
+    var news = News.find({}, {sort: {date: -1}, limit: 1});
+    return news.count() && news.fetch()[0].text;
   }
 });
--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,10 @@ Meteor.publish('allRecipes', function() {
   return Recipes.find();
 });
 
+Meteor.publish('news', function() {
+  return News.find({}, {sort: {date: -1}, limit: 1});
+});
+
 Meteor.publish('feed', function() {
   return Activities.find({}, {sort: {date: -1}, limit: 10});
 });
