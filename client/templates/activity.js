c7ec71577c804c7615981c7321d3c5bc06be9448
Zoltan Olah <zol@percolatestudio.com>
2014-10-20 18:11:05 -0700

    Temporary hack for scrolling activity into view for #21

--- client/templates/activity.js
+++ client/templates/activity.js
@@ -1,9 +1,13 @@
 Template.activity.rendered = function() {
-  // XXX: scrollIntoView seemingly messes up the L-R transition :-/ we need
-  // to fix it
+  var self = this;
+
+  // XXX: See https://github.com/percolatestudio/mdg-recipes/issues/21 for a description of the magic '100' number temporary 'hack'
   // If we're the activity in a list somewhere, scroll us into view
-  // if (Router.current().params.activityId === this.data._id)
-  //   this.firstNode.scrollIntoView();
+  if (Router.current().params.activityId === self.data._id) {
+    Meteor.setTimeout(function() {
+      self.firstNode.scrollIntoView();
+    }, 100);
+  }
 }
 
 Template.activity.helpers({
