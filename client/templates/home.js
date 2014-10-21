7837b39312281e087f0094478cea5b3e0f8e171e
Zoltan Olah <zol@percolatestudio.com>
2014-10-21 11:51:29 -0700

    Selects featured recipes at random

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,6 +1,15 @@
+var FEATURED_COUNT = 4;
+
 Template.home.helpers({
+  // selects FEATURED_COUNT number of recipes at random
   featuredRecipes: function() {
-    return _.values(RecipesData).slice(0, 4);
+    var recipes = _.values(RecipesData);
+    var selection = [];
+    
+    for (var i = 0;i < FEATURED_COUNT;i++)
+      selection.push(recipes.splice(_.random(recipes.length - 1), 1)[0]);
+
+    return selection;
   },
   
   activities: function() {
