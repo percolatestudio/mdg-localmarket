1146fd96bcda4fd977ed12e589e07c81576ea251
Tom Coleman <tom@nala.local>
2014-10-21 14:55:34 +1100

    Hand-code `scrollIntoView()` so it doesn’t go sideways #21

--- client/templates/activity.js
+++ client/templates/activity.js
@@ -4,9 +4,11 @@ Template.activity.rendered = function() {
   // XXX: See https://github.com/percolatestudio/mdg-recipes/issues/21 for a description of the magic '100' number temporary 'hack'
   // If we're the activity in a list somewhere, scroll us into view
   if (Router.current().params.activityId === self.data._id) {
-    Meteor.setTimeout(function() {
-      self.firstNode.scrollIntoView();
-    }, 100);
+    var $activity = $(self.firstNode);
+    var top = $activity.offset().top;
+    var $parent = $(self.firstNode).closest('.content-scrollable');
+    var parentTop = $parent.offset().top;
+    $parent.scrollTop(top - parentTop);
   }
 }
 
