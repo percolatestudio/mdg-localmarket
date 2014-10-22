e3e05440eeadee314cd09d68e450be74dbc515fb
Dominic <dom@percolatestudio.com>
2014-10-22 15:34:03 -0700

    Cordova: new splash images

--- mobile-config.js
+++ mobile-config.js
@@ -23,23 +23,23 @@ App.icons({
 
 App.launchScreens({
   // iOS
-  'iphone': 'resources/splash/splash-320x480.jpg',
-  'iphone_2x': 'resources/splash/splash-320x480@2x.jpg',
-  'iphone5': 'resources/splash/splash-320x568@2x.jpg',
-  'ipad_portrait': 'resources/splash/splash-768x1024.jpg',
-  'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.jpg',
-  'ipad_landscape': 'resources/splash/splash-1024x768.jpg',
-  'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.jpg',
+  'iphone': 'resources/splash/splash-320x480.png',
+  'iphone_2x': 'resources/splash/splash-320x480@2x.png',
+  'iphone5': 'resources/splash/splash-320x568@2x.png',
+  'ipad_portrait': 'resources/splash/splash-768x1024.png',
+  'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
+  'ipad_landscape': 'resources/splash/splash-1024x768.png',
+  'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
 
   // Android
-  'android_ldpi_portrait': 'resources/splash/splash-200x320.jpg',
-  'android_ldpi_landscape': 'resources/splash/splash-320x200.jpg',
-  'android_mdpi_portrait': 'resources/splash/splash-320x480.jpg',
-  'android_mdpi_landscape': 'resources/splash/splash-480x320.jpg',
-  'android_hdpi_portrait': 'resources/splash/splash-480x800.jpg',
-  'android_hdpi_landscape': 'resources/splash/splash-800x480.jpg',
-  'android_xhdpi_portrait': 'resources/splash/splash-720x1280.jpg',
-  'android_xhdpi_landscape': 'resources/splash/splash-1280x720.jpg'
+  'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
+  'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
+  'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
+  'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
+  'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
+  'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
+  'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
+  'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
 });
 
 App.setPreference('StatusBarOverlaysWebView', 'false');
